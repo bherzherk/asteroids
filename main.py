d40ce24a32ca49a4345d25de1c2b5ad1ef0c4e50
bherzherk <bherzherk@gmail.com>
2025-08-20 19:36:07 -0400

    Grouping updatable and drawable

--- main.py
+++ main.py
@@ -11,14 +11,23 @@ def main():
     clock = pygame.time.Clock() # setting clock for fps
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
         screen.fill(black)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip() # update display
         dt = clock.tick(60) / 1000 # limit to 60 fps
 
