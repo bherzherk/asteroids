613aae3926cb201c0bdfb07c20a4cb04dfe4c186
bherzherk <bherzherk@gmail.com>
2025-08-19 22:39:10 -0400

    Added rotation in player module, CircleSprite constructor fixed

--- main.py
+++ main.py
@@ -9,23 +9,18 @@ def main():
     black = (0, 0, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock() # setting clock for fps
-    player = Player(SCREEN_HEIGHT/2, SCREEN_HEIGHT/2)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
         screen.fill(black)
         player.draw(screen)
         pygame.display.flip() # update display
         dt = clock.tick(60) / 1000 # limit to 60 fps
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__ == "__main__":
     main()
